6401d6c5f5d12b579466b32c1fb8f3b13d4a361a
AbelSegawa439 <ssegawaabel2@gmail.com>
2026-08-18 11:51:20 +0300

    Create apps/pwa/capacitor.config.ts

---
+++ apps/pwa/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "ug.stcatherine.eschool",
+  appName: "St Catherine SS",
+  webDir: "dist",
+};
+
+export default config;
